9d8114e6f47230d0907f448dad4f2b42d6d9b71f
Alberto De La Torre <luis.atorred24@gmail.com>
2022-05-05 16:22:47 -0400

    feat: Dynamic Menu in the Header component

---
+++ mushroomsoft/app/src/app/constants.ts
@@ -0,0 +1,38 @@
+export const Menu = {
+  logo: [
+    {
+      image: 'https://uploads-ssl.webflow.com/6062651bd170589a75c3b5f0/6062651bd170582278c3b612_logo-footer.png',
+      classA: 'ms-logo-link ms-nav-brand ms--current',
+      classImage: 'ms-logo-image',
+      width: '150',
+      url: '#'
+    }
+  ],
+  menu: [
+    {
+      title: 'WHO WE ARE',
+      class: 'ms-navigation-item ms-nav-link',
+      url: '#Whoweare'
+    },
+    {
+      title: 'SERVICES',
+      class: 'ms-navigation-item ms-nav-link',
+      url: '#services'
+    },
+    {
+      title: 'COMMITMENT',
+      class: 'ms-navigation-item ms-nav-link',
+      url: '#focus'
+    },
+    {
+      title: 'TECHNOLOGY',
+      class: 'ms-navigation-item ms-nav-link',
+      url: '#tecnology'
+    },
+    {
+      title: 'LOCATION',
+      class: 'ms-navigation-item ms-nav-link',
+      url: '#location'
+    }
+  ]
+};
