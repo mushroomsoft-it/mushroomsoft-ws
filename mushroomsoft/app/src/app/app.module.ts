653be13fd34f1bef4e56902addd42e716491f6a5
Alberto De La Torre <luis.atorred24@gmail.com>
2022-05-05 12:17:54 -0400

    feat: PrimeNG library added

--- mushroomsoft/app/src/app/app.module.ts
+++ mushroomsoft/app/src/app/app.module.ts
@@ -3,10 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
