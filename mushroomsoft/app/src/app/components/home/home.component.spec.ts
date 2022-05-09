4f43c62a7f59afbf8085d756ec6dcbb834239653
Alberto De La Torre <luis.atorred24@gmail.com>
2022-05-09 15:12:02 -0400

    feat:The commitment section

---
+++ mushroomsoft/app/src/app/components/home/home.component.spec.ts
@@ -0,0 +1,16 @@
+import {HomeComponent} from './home.component';
+
+describe('HomeComponent', () => {
+  it('should initialization of the component', () => {
+    const component = new HomeComponent();
+    expect(component).toBeDefined();
+    expect(component).toBeInstanceOf(HomeComponent);
+    expect(component.sectionOne.length).toBe(1);
+    expect(component.sectionTwo.length).toBe(1);
+    expect(component.sectionThree.length).toBe(1);
+    expect(component.commitmentTitle.length).toBe(1);
+    expect(component.commitmentImage.length).toBe(1);
+    expect(component.commitmentDescription.length).toBe(1);
+
+  });
+});
---
+++ mushroomsoft/app/src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import sectionItems from '../../../config/section.json';
+import commitmentItems from '../../../config/commitment.json';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  public sectionOne = sectionItems.section.one;
+  public sectionTwo = sectionItems.section.two;
+  public sectionThree = sectionItems.section.three;
+  public commitmentTitle = commitmentItems.commitment.title;
+  public commitmentImage = commitmentItems.commitment.image;
+  public commitmentDescription = commitmentItems.commitment.description;
+}
