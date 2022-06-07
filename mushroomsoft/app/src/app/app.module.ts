3d4b8ddeefd66f40109e2744ff123d9857599ae6
Alberto De La Torre <luis.atorred24@gmail.com>
2022-06-07 12:51:27 -0400

    feat:Create the component to have the form UI

--- mushroomsoft/app/src/app/app.module.ts
+++ mushroomsoft/app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormlyModule} from '@ngx-formly/core';
 import {AppRoutingModule} from './app-routing.module';
+import {FormlyBootstrapModule} from '@ngx-formly/bootstrap';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 
 import {HomeComponent} from './components/home/home.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ContactComponent],
 
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [CommonModule, BrowserModule, AppRoutingModule, ReactiveFormsModule, FormlyModule.forRoot(), FormlyBootstrapModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ mushroomsoft/app/src/app/components/contact/contact.component.spec.ts
@@ -0,0 +1,10 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ContactComponent} from './contact.component';
+
+describe('ContactComponent', () => {
+  it('should initialization of the component', () => {
+    const component = new ContactComponent();
+    component.submit();
+  });
+});
