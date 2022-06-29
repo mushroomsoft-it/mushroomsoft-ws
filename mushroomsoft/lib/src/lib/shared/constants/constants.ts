475efe54231cf9b6e519d396fee7e437b78dc24c
Alberto De La Torre <luis.atorred24@gmail.com>
2022-06-29 12:35:28 -0400

    fix:comments resolved

--- mushroomsoft/lib/src/lib/shared/constants/constants.ts
+++ mushroomsoft/lib/src/lib/shared/constants/constants.ts
@@ -1,1 +1,2 @@
-export const API_STRAPI = process.env['NG_APP_STRAPI_API_URL'] + 'forms?filters[type][$eq]=';
+//export const API_STRAPI = process.env['NG_APP_STRAPI_API_URL'] + 'forms?filters[type][$eq]=';
+export const API_STRAPI = 'http://3.86.26.7:1337/api/forms?filters[type][$eq]=';
